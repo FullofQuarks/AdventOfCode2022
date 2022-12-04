735f48a2c0d1b1f7929d46ad2b5f2ec5be499f90
Nicholas Smith <nsmith@ethosgroup.com>
2022-12-04 02:02:11 -0600

    Attempt on day 2

--- src/days/day_02/mod.rs
+++ src/days/day_02/mod.rs
@@ -1,3 +1,22 @@
 pub fn execute() {
-    
+    let mut games: Vec<u32> = vec![0];
+    let mut current_game = 0;
+    if let Ok(lines) = crate::utilities::read_lines("src/days/day_02/02-Test.txt") {
+        for line in lines {
+            if let Ok(game) = &line {
+                let string = line.unwrap();
+                let mut chars = string.char_indices();
+
+                // nth consumes all chars to the index
+                let opponent_move = chars.nth(0).unwrap().1;
+                let suggested_move = chars.nth(1).unwrap().1;
+                if opponent_move == 'A' {
+                    if suggested_move == 'Y' {
+                        games[current_game] = 6;
+                    }
+                }
+            }
+            current_game += 1;
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,10 @@ fn main() {
     println!("Input a day: ");
     let mut choice = String::new();
     std::io::stdin().read_line(&mut choice).expect("msg");
+    println!("Executing Day {} of Santa Protocol...", &choice.trim());
     match &choice.trim()[..] {
         "1" => crate::days::day_01::execute(),
+        "2" => crate::days::day_02::execute(),
         _ => println!("None chosen. Exiting.")
     }
 }
